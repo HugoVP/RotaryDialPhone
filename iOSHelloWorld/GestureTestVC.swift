ec4119552fb66253c8c1daf9622f1f7dd21af551
Hugo <hugo@Tsukuyomi.lan>
2016-12-13 18:04:20 -0600

    save

--- iOSHelloWorld/GestureTestVC.swift
+++ iOSHelloWorld/GestureTestVC.swift
@@ -9,23 +9,44 @@
 import UIKit
 
 class GestureTestVC: UIViewController, UIGestureRecognizerDelegate {
-    
+ 
     @IBOutlet weak var syncBtn: UIButton!
-    @IBOutlet weak var syncImageView: UIImageView!   
+    @IBOutlet weak var syncImageView: UIImageView!
    
     var angle = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let tapSyncBtn = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
         
-        syncBtn.addGestureRecognizer(tapSyncBtn)
+        _ = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(self .fire),
+            userInfo: nil,
+            repeats: true)
+        
+//        let timer = Timer.scheduledTimer(
+//            timeInterval: 1.0,
+//            target: self,
+//            selector: #selector(fire(_:)),
+//            userInfo: nil,
+//            repeats: true)
+
+//        let tapSyncBtn = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        
+//        syncBtn.addGestureRecognizer(tapSyncBtn)
+    }
+    
+//    @IBAction func fire (_ timer: Timer) {
+    func fire () {
+        print(angle)
+        syncImageView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+        angle += 0.0174533 * 25.7142857143
     }
     
     func startSpinning () {
         syncImageView.transform = CGAffineTransform(rotationAngle: CGFloat(/*M_PI / 2)*/angle))
-        angle += 0.0174533 * 15.0
+        angle += 0.0174533 * 25.7142857143
     }
     
     @IBAction func handleTap (_ sender: UITapGestureRecognizer) {
