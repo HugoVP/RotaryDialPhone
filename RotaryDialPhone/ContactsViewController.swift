ae7c3cecc3af839b0ec95d4a487b079bc1ee2f10
jose carabez <jay_ge_2ce@hotmail.com>
2017-12-17 16:34:13 -0600

    New view contact

--- RotaryDialPhone/ContactsViewController.swift
+++ RotaryDialPhone/ContactsViewController.swift
@@ -9,35 +9,81 @@
 import UIKit
 import Contacts
 
-class ContactsViewController: UIViewController {
-
-
+class ContactsViewController: UIViewController, UISearchResultsUpdating, UISearchBarDelegate {
+    
+    @IBOutlet weak var numberContactLabel: UILabel!
+    @IBOutlet weak var nameContactLabel: UILabel!
+    
+    let keys = [CNContactIdentifierKey as CNKeyDescriptor,
+                CNContactPhoneNumbersKey as CNKeyDescriptor,
+                CNContactFormatter.descriptorForRequiredKeys(for:CNContactFormatterStyle.fullName)]
+    let formatter = CNContactFormatter()
+    let contactStore = CNContactStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.formatter.style = .fullName
+        self.numberContactLabel.text = ""
+        self.nameContactLabel.text = ""
+        requestForAccess { (accessGranted) in
+            // Nothing here
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func onClick(_ sender: UIButton) {
-        print("Click desde contactos")
-        print("Test")
-    }
-
-    func requestForAccess(completionHandler: (_ accessGranted: Bool) -> Void) {
-        // Get authorization
+    func updateSearchResults(for searchController: UISearchController) {
         let authorizationStatus = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
-
+        if let inputText = (searchController.searchBar.text), !inputText.isEmpty, authorizationStatus == .authorized {
+            searchContact(input: inputText)
+        }
+    }
+    
+    func searchContact(input: String) {
+        self.nameContactLabel.alpha = 0;
+        self.numberContactLabel.alpha = 0;
+        //requestForAccess { (accessGranted) in
+            //if accessGranted {
+                let predicate = CNContact.predicateForContacts(matchingName: input)
+                do {
+                    let contacts = try self.contactStore.unifiedContacts(matching: predicate, keysToFetch: self.keys)
+                    if(contacts.count == 0) {
+                        //DispatchQueue.main.async {
+                        self.nameContactLabel.text = "No contact found."
+                        self.numberContactLabel.text = ""
+                        //}
+                    } else {
+                        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseIn, animations: {
+                            self.nameContactLabel.alpha = 1;
+                            self.numberContactLabel.alpha = 1;
+                            self.nameContactLabel.text = self.formatter.string(from: contacts[0])!
+                            self.numberContactLabel.text = contacts[0].phoneNumbers.first!.value.stringValue
+                        }, completion: nil);
+                    }
+                } catch {
+                    print("Unable to fetch contacts")
+                }
+            //}
+        //}
+    }
+    
+    func requestForAccess(completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+        let authorizationStatus = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
         switch authorizationStatus {
         case .authorized:
-            break
+            completionHandler(true)
         case .denied, .notDetermined:
-            break
+            CNContactStore().requestAccess(for: CNEntityType.contacts, completionHandler: { (access, error) -> Void in
+                if(access){
+                    completionHandler(access)
+                } else if(authorizationStatus == CNAuthorizationStatus.denied) {
+                    /*DispatchQueue.main.async {
+                        let message = "accessError!.localizedDescription)\n\nPlease allow the app to access your contacts through the Settings."
+                        self.showMessage(msg: message)
+                    }*/
+                    completionHandler(false)
+                }
+            })
         default:
-            break
+            completionHandler(false)
         }
     }
 
--- RotaryDialPhone/MainViewController.swift
+++ RotaryDialPhone/MainViewController.swift
@@ -9,114 +9,37 @@
 import UIKit
 import Contacts
 
-class MainViewController: UIViewController, UISearchResultsUpdating, UISearchBarDelegate {
-
-    @IBOutlet weak var nameContactLabel: UILabel!
-    @IBOutlet weak var numberContactLabel: UILabel!
-
-    let keys = [CNContactIdentifierKey as CNKeyDescriptor,
-                CNContactPhoneNumbersKey as CNKeyDescriptor,
-                CNContactFormatter.descriptorForRequiredKeys(for:CNContactFormatterStyle.fullName)]
-    let formatter = CNContactFormatter()
-    let contactStore = CNContactStore()
-
-    //var contacts = [CNContact]()
-
+class MainViewController: UIViewController {
+    
+    var contactViewController: ContactsViewController? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.formatter.style = .fullName
-        self.nameContactLabel.text = ""
-        self.numberContactLabel.text = ""
         let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = self
-      if #available(iOS 9.1, *) {
-        searchController.obscuresBackgroundDuringPresentation = false
-      } else {
-        // Fallback on earlier versions
-      }
+        searchController.searchResultsUpdater = self.contactViewController
+        if #available(iOS 9.1, *) {
+            searchController.obscuresBackgroundDuringPresentation = false
+        }
         searchController.searchBar.placeholder = "Nombre del contacto"
-        searchController.searchBar.delegate = self
+        searchController.searchBar.delegate = self.contactViewController
+        searchController.searchBar.showsCancelButton = false
+        
         searchController.searchBar.sizeToFit()
         if #available(iOS 11.0, *) {
             navigationItem.searchController = searchController
-        } else {
+         } else {
             navigationItem.titleView = searchController.searchBar
         }
         definesPresentationContext = true
     }
-
-    func updateSearchResults(for searchController: UISearchController) {
-        //let authorizationStatus = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
-        if let inputText = (searchController.searchBar.text), !inputText.isEmpty {
-            searchContact(input: inputText)
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let contactViewController = segue.destination as? ContactsViewController {
+            self.contactViewController = contactViewController
         }
     }
-
-    func searchContact(input: String) {
-        self.nameContactLabel.alpha = 0;
-        self.numberContactLabel.alpha = 0;
-        DispatchQueue.global().async {
-            let predicate = CNContact.predicateForContacts(matchingName: input)
-            do {
-                let contacts = try self.contactStore.unifiedContacts(matching: predicate, keysToFetch: self.keys)
-                if(contacts.count == 0) {
-                    DispatchQueue.main.async {
-                        self.nameContactLabel.text = "No contact found."
-                        self.numberContactLabel.text = ""
-                    }
-                } else {
-                    for contact in contacts {
-                        print(self.formatter.string(from: contact)!)
-                    }
-                    DispatchQueue.main.async {
-                        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseIn, animations: {
-                            self.nameContactLabel.alpha = 1;
-                            self.numberContactLabel.alpha = 1;
-                            self.nameContactLabel.text = self.formatter.string(from: contacts[0])!
-                            self.numberContactLabel.text = contacts[0].phoneNumbers.first!.value.stringValue
-                        }, completion: nil);
-                    }
-                }
-            } catch {
-                print("Unable to fetch contacts")
-            }
-        }
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-
-    func requestForAccess(completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
-        let authorizationStatus = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
-
-        switch authorizationStatus {
-        case .authorized:
-            completionHandler(true)
-        case .denied, .notDetermined:
-            CNContactStore().requestAccess(for: CNEntityType.contacts, completionHandler: { (access, error) -> Void in
-                if(access){
-                    completionHandler(access)
-                }else if(authorizationStatus == CNAuthorizationStatus.denied){
-                    DispatchQueue.main.async {
-                        let message = "accessError!.localizedDescription)\n\nPlease allow the app to access your contacts through the Settings."
-                        self.showMessage(msg: message)
-                    }
-                }
-            })
-        default:
-            completionHandler(false)
-        }
-    }
-
-    func showMessage(msg: String) {
-        let alertController = UIAlertController(title: "Alert", message: msg, preferredStyle: UIAlertControllerStyle.alert)
-        let dismissAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action) -> Void in }
-        alertController.addAction(dismissAction)
-        self.present(alertController, animated: true, completion: nil)
-    }
-
-    func requestContactsPermition() {
-
     }
 }
