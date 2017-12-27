a5a5734e33734e8dee34915bb7f539a3e3b79b43
HugoVP <dev@hugo.plus>
2017-12-27 12:35:20 -0600

    Settings View Controller added

---
+++ RotaryDialPhone/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RotaryDialSettingsViewController.swift
+//  RotaryDialPhone
+//
+//  Created by Hugo on 26/12/17.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
