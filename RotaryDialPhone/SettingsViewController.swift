4a09b9dfd1db8c03c1e2de51f23eeaddbba547f5
HugoVP <dev@hugo.plus>
2017-12-28 20:41:35 -0600

    Skin Selection View Controller added

--- RotaryDialPhone/SettingsViewController.swift
+++ RotaryDialPhone/SettingsViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    tableView.dataSource = self
+  }
+}
+
+extension SettingsViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "SkinSelectionTableViewCell", for: indexPath)
+    return cell
   }
 }
---
+++ RotaryDialPhone/SkinSelectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SkinSelectionViewController.swift
+//  RotaryDialPhone
+//
+//  Created by Hugo on 28/12/17.
+//
+
+import UIKit
+
+class SkinSelectionViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
